3476127c9abb5ac49336312981c7000a4b58986a
delfisProject <louveau.cdi04@angers.imie>
2017-05-23 16:33:46 +0200

    ajout test pour get

---
+++ testHttps.py
@@ -0,0 +1,19 @@
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+import os
+import coucou
+
+class RedirectHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+		def do_GET(s):
+            s.send_response(200)
+    		s.send_header('Content-type','text/html')
+    		s.end_headers()
+    		# Send the html message
+    		s.wfile.write("Hello World !")
+    		return
+		def do_POST(s):
+			print coucou.main('post')
+
+httpd = BaseHTTPServer.HTTPServer(('', 4443), RedirectHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='/etc/ssl/server.pem', server_side=True)
+httpd.serve_forever()
