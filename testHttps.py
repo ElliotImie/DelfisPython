f82f5296a080eed3f988f6fc38d25d782f9bea71
delfisProject <louveau.cdi04@angers.imie>
2017-05-23 16:36:04 +0200

    ajout test pour get2

--- testHttps.py
+++ testHttps.py
@@ -6,13 +6,13 @@ import coucou
 class RedirectHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 		def do_GET(s):
             s.send_response(200)
-    		s.send_header('Content-type','text/html')
-    		s.end_headers()
-    		# Send the html message
-    		s.wfile.write("Hello World !")
-    		return
-		def do_POST(s):
-			print coucou.main('post')
+            s.send_header('Content-type','text/html')
+            s.end_headers()
+            # Send the html message
+            s.wfile.write("Hello World !")
+            return
+        def do_POST(s):
+            print coucou.main('post')
 
 httpd = BaseHTTPServer.HTTPServer(('', 4443), RedirectHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='/etc/ssl/server.pem', server_side=True)
