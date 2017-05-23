fdb13bd0542ac0f14ace81de4421e543bb220cd5
delfisProject <louveau.cdi04@angers.imie>
2017-05-23 15:36:55 +0200

    clean dossier

--- https.py
+++ https.py
@@ -8,7 +8,7 @@ class RedirectHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			return coucou.main('ta fait un get frere')
 		def do_POST(s):
 			print coucou.main('post')
-		
+
 httpd = BaseHTTPServer.HTTPServer(('', 4443), RedirectHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='/etc/ssl/server.pem', server_side=True)
 httpd.serve_forever()
