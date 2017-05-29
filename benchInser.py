0c58f99e0f0595638407211622b957abf77cbc20
delfisProject <louveau.cdi04@angers.imie>
2017-05-29 12:07:47 +0200

    change bench.py

--- benchInser.py
+++ benchInser.py
@@ -8,14 +8,14 @@ import script_velo
 connBDD = mysql.connector.connect(host="localhost", user="root", password="delfis", database="delfis")
 cursor = connBDD.cursor()
 
-# Pour bench insert
-# cursor.execute("""
-#     CREATE TABLE IF NOT EXISTS usr_32(
-#     id varchar(20) NOT NULL,
-#     latitude decimal(10,8) NOT NULL,
-#     longitude decimal(10,8) NOT NULL
-# );
-# """)
+Pour bench insert
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS usr_32(
+    id varchar(20) NOT NULL,
+    latitude decimal(10,8) NOT NULL,
+    longitude decimal(10,8) NOT NULL
+);
+""")
 
 lat = 10.0001
 lng = 5.0001
@@ -29,20 +29,20 @@ timedebut  = time.time()
 while i < 10000 :
     time1 = time.time()
 
-#Pour Bench insert :
-    # cursor.execute('INSERT INTO usr_'+str(dept)+'(id,latitude,longitude) VALUES ("test",'+ str(lat)+' , '+str(lng)+');')
-    # connBDD.commit()
+Pour Bench insert :
+    cursor.execute('INSERT INTO usr_'+str(dept)+'(id,latitude,longitude) VALUES ("test",'+ str(lat)+' , '+str(lng)+');')
+    connBDD.commit()
 
 #Pour bench script_velo :
-    script_velo.main(jsonTest)
+    # script_velo.main(jsonTest)
 
     time2 = time.time()
 
     print("insert OK en : " + str(time2 - time1) +"sec")
 
 #Pour bench insert
-    # lat += 0.0002
-    # lng += 0.0002
+    lat += 0.0002
+    lng += 0.0002
     i += 1
 
 timefin = time.time()
